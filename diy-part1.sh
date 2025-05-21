cbf70d58a908d2b50e0b9371fb48850cc203c268
tisny886 <71377748+tisny886@users.noreply.github.com>
2025-05-22 04:31:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git kenzok8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 
 PKG_PATH="$GITHUB_WORKSPACE/wrt/package/"
 
